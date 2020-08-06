cb733baf6dd9d1276edb9a7d66103f4ca4f6caef
nikita <nikita.gurin2018@yandex.ru>
2020-08-06 12:18:30 +0300

    Laba1

---
+++ ShopPCBusinessLogic/BindingModels/ChangeStatusBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopPCBusinessLogic.BindingModels
+{
+    public class ChangeStatusBindingModel
+    {
+        public int OrderId { get; set; }
+    }
+}
---
+++ ShopPCBusinessLogic/ViewModels/ComponentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ShopPCBusinessLogic.ViewModels
+{
+    public class ComponentViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Название продукта")]
+        public string ComponentName { get; set; }
+    }
+}
